d3384da8f01a4a7dd949589238c57f313e2ab7ea
wagnermarques <wagnerdocri@gmail.com>
2026-09-02 20:28:37 -0300

    Criação de um novo componente

--- src/componentes/lib.js
+++ src/componentes/lib.js
@@ -1,5 +1,6 @@
 import { menuClass } from "./menu/menu.js"
 import{ footerClass } from "./footer/footer.js"
-
+import { modalidadeClass } from "./modalidades/modalidades.js";
 customElements.define('tcc-menu' , menuClass); 
 customElements.define('tcc-footer' , footerClass); 
+customElements.define('tcc-modalidades', modalidadeClass)
